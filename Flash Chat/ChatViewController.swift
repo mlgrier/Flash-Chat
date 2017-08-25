a55bf13b423af8f03b7eb76427b6cfc3a09c7f55
Marco Grier <mlgrier@gmail.com>
2017-08-24 21:21:59 -0500

    updated

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -59,9 +59,9 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Message", "Third Message"]
-        
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         
         return cell
         
